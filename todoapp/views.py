264f9c4c8de5c8f9e11dc67e2f98990750cd0aec
pr454d-p <7697prasad@gmail.com>
2023-04-01 22:38:22 +0530

    removed class views

--- todoapp/views.py
+++ todoapp/views.py
@@ -3,80 +3,10 @@ from django.contrib.auth.decorators import login_required
 from datetime import date
 from .models import Task
 from . forms import TodoForm
-from django.views.generic import ListView
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import UpdateView,DeleteView,CreateView
-from django.urls import reverse_lazy
-
-# Create your views here.
-class TaskListView(ListView):
-    model = Task
-    template_name = 'home.html'
-    context_object_name = 'tasks'
-    fields = ('title','priority','date')
-
-class TaskCreateView(CreateView):
-    model = Task
-    template_name = 'create.html'
-    context_object_name = 'tasks'
-    fields = ('title','priority','date')
-    success_url = reverse_lazy('cbvhome')
 
 
-class TaskDetailView(DetailView):
-    model = Task
-    template_name = 'detail.html'
-    context_object_name = 'task'
 
-class TaskUpdateView(UpdateView):
-    model = Task
-    template_name = 'update.html'
-    context_object_name = 'task'
-    fields = ('title','priority','date')
-
-    def get_success_url(self):
-        return reverse_lazy('cbvdetail',kwargs= {'pk': self.object.id})
-
-
-class TaskDeleteView(DeleteView):
-    model = Task
-    template_name = 'delete.html'
-    success_url = reverse_lazy('cbvhome')
-
-# @login_required
-# def index(request):
-#     user = request.user
-#     tasks = Task.objects.all()
-#     if request.method == 'POST':
-#         form = TodoForm(request.POST)
-#         if form.is_valid():
-#             # Save the form data to the database
-#             todo_item = form.save(commit=False)
-#             todo_item.user = request.user
-#             todo_item.save()
-#             return redirect('/')
-#         # title = request.POST.get('title','')
-#         # priority = request.POST.get('priority','')
-#         # date = request.POST.get('date','')
-#         # todo_item.user = request.user
-#         # task = Task(title= title,priority = priority,date=date)
-#         # task.save()
-#     return render(request,'index.html',{'tasks': tasks})
-# @login_required(login_url='login')
-# def index(request):
-#     user = request.user
-#     tasks = Task.objects.filter(user=user)
-#     if request.method == 'POST':
-#         # Save the form data to the database
-#         title = request.POST.get('title','')
-#         priority = request.POST.get('priority','')
-#         date = request.POST.get('date','')
-#         user = request.user
-#         task = Task(title= title,priority = priority,date=date,user=user)
-#         task.save()
-#     return render(request,'index.html',{'tasks': tasks})
-
-@login_required(login_url='account/login')
+@login_required(login_url='account:login')
 def index(request):
     user = request.user
     tasks = Task.objects.filter(user=user).order_by('priority')
@@ -109,4 +39,4 @@ def update(request,id):
         form.save()
         return redirect('/')
 
-    return render(request,'edit.html',{'form':form})
+    return render(request,'update.html',{'form':form})
