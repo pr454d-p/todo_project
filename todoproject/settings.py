b8ea0d407a30d63261824cd59e2c16dcdb50570e
pr454d-p <7697prasad@gmail.com>
2023-04-01 23:03:50 +0530

    gmail

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -123,12 +123,12 @@ STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
-EMAIL_HOST_USER = 'todoslists@outlook.com' 
-EMAIL_HOST = 'smtp-mail.outlook.com'
-EMAIL_PORT = 587 
-EMAIL_USE_TLS = True 
-EMAIL_HOST_PASSWORD = '9633@Jan'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = 'mextechies@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = 'evekukpkegdaoiag'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
